8515698005f5399b95d9831cbee01105bfcc27ad
蔡冲冲 <cigg153@163.com>
2017-04-29 17:23:04 +0800

    @Mapper注解

--- src/main/java/com/ccc/dao/mapper/SysUserMapper.java
+++ src/main/java/com/ccc/dao/mapper/SysUserMapper.java
@@ -1,10 +1,12 @@
 package com.ccc.dao.mapper;
 
 import com.ccc.entity.SysUser;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * Created by ccc on 2017/4/29.
  */
+@Mapper
 public interface SysUserMapper {
     int modifySysUser(SysUser sysuser);
 }
